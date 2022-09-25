c3e175a2dc66e5450697a1f3b76e9d826b9091bd
Dawid Poniewierka <dawid.poniewierka@gmail.com>
2022-09-25 16:18:33 +0200

    added js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+'use strict';
+// DOM
+const btn = document.querySelector('header a');
+const h1 = document.querySelector('header h1');
+let counter = 0;
+
+btn.addEventListener('click', () => {
+    counter++;
+    console.log(counter);
+    if(counter % 2 === 0){
+        h1.innerHTML = 'Salon Samochodowy w Krakowie';
+    } else {
+        h1.innerHTML = 'Zapraszamy do odwiedzin';
+    }
+    
+})
